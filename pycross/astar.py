1a707c2ed657eb9a1b12a59f684f3cb054a362c9
Edvinas Teiserskis <teiserse@tcd.ie>
2021-04-11 00:42:06 +0100

    Actually fixed checker this time i hope

--- pycross/astar.py
+++ pycross/astar.py
@@ -1,6 +1,7 @@
 from queue import PriorityQueue
 import pycross
 
+
 # notes
 # - start with a dumb queue and go from there.
 # - check the a* notes to figure out a good heuristic.
@@ -17,6 +18,7 @@ import pycross
 def perform_search(puzzle: pycross.Picross):
     # Setup
     frontier = PriorityQueue()
+    already_input = set()
 
     # Prepare start
     # is it already solved?
@@ -24,3 +26,57 @@ def perform_search(puzzle: pycross.Picross):
         return puzzle
 
     # set up the first set of actions
+    for row in range(puzzle.height):
+        for column in range(puzzle.width):
+            for colour in puzzle.colours.keys():
+                filled_tiles = set()
+                filled_tiles.add((row, column, colour))
+                frontier.put((1, filled_tiles))
+                already_input.add(frozenset(filled_tiles))
+
+    # Begin search
+    found = False
+    while not found:
+        # get the set of used tiles
+        _weight, inputs = frontier.get()
+        # set up the game board
+        # also, get the set of covered tiles
+        used_tiles = set()
+        for item in inputs:
+            row, column, colour = item
+            puzzle[row][column] = colour
+            used_tiles.add((row, column))
+        # check if it is solved
+        if puzzle.is_complete():
+            found = True
+            print(puzzle)
+            break
+        # if not solved, reset the board
+        for item in inputs:
+            row, column, _ = item
+            puzzle[row][column] = -1
+        # add to the frontier
+        for row in range(puzzle.height):
+            for column in range(puzzle.width):
+                for colour in puzzle.colours.keys():
+                    if (row, column) not in used_tiles:
+                        new_inputs = inputs.copy()
+                        new_inputs.add((row, column, colour))
+                        if frozenset(new_inputs) not in already_input:
+                            frontier.put((len(new_inputs), new_inputs))
+                            already_input.add(frozenset(new_inputs))
+        if frontier.empty():
+            print("No Solution Found")
+            found = True
+
+    # found solution
+    return puzzle
+
+
+if __name__ == '__main__':
+    x = pycross.Picross(5, 2)
+    x.colours = {1: "black"}
+    x.rows = [[[1, 2], [1, 2]], [[1, 5]]]
+    x.columns = [[[1, 2]], [[1, 2]], [[1, 1]], [[1, 2]], [[1, 2]]]
+    y = perform_search(x)
+    print(y)
