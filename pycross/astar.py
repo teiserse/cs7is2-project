a23b3994c243f4efc04a7d463527d2c53c0c19cb
Edvinas Teiserskis <teiserse@tcd.ie>
2021-04-10 19:48:17 +0100

    Start of astar

--- pycross/astar.py
+++ pycross/astar.py
@@ -10,7 +10,17 @@ import pycross
 # - remember - this is not going to be good because search is bad for this type of thing.
 # - Encode the solution as a set of filling in because order doesn't matter? also avoids directly needing the 2D grid
 # - - a tuple of (x, y, colour)?
+# - currently will be fed in an empty puzzle. Might be changed later in order to take in half-complete ones
+# or even potentially wrongly filled ones (the last one will be quite hard, but possible?).
 
 
-def perform_search(puzzle):
-    pass
+def perform_search(puzzle: pycross.Picross):
+    # Setup
+    frontier = PriorityQueue()
+
+    # Prepare start
+    # is it already solved?
+    if puzzle.is_complete():
+        return puzzle
+
+    # set up the first set of actions
