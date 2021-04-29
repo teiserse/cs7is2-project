517fdefe05d82929bf215e2ad09ac699505a40be
Eddy <chenge@tcd.ie>
2021-04-29 19:31:03 +0100

    Fixed spelling

--- pycross/dispImage.py
+++ pycross/dispImage.py
@@ -8,33 +8,37 @@ class GridFrame(wx.Frame, object):
 
         # Create a wxGrid object
         self.grid = wx.grid.Grid(self, -1)
+
+        # Then we call CreateGrid to set the dimensions of the grid
+        # (100 rows and 10 columns in this example)
         self.grid.CreateGrid(10, 10)
 
         for row in range(10):
             self.grid.SetRowSize(row, 30)
             self.grid.SetColSize(row, 30)
 
-    def set_white(self,puzzle):
+    def setWhite(self,puzzle):
         for r in range(puzzle.height):
             for c in range(puzzle.width):
                 self.grid.SetCellBackgroundColour(r,c,wx.WHITE)
 
-    def set_colour(self,puzzle):
-        colours = []
-        for item in puzzle.colours:
-            colours.append(puzzle.colours[item])
-        for colour in puzzle.colours.keys():
-            for row in range(puzzle.height):
-                for col in range(puzzle.width):
-                    if puzzle[row][col] == colour:
-                        self.grid.SetCellBackgroundColour(row,col,puzzle.colours[colour])
+    def set_colour(self, puzzle):
+        colour_dict = pycross.from_json(open("colourDict.json").read())
+        colours = puzzle.colours
+        for key in colours:
+            for colour_def in colour_dict.keys():
+                if colours[key] == colour_def:
+                    for row in range(puzzle.height):
+                        for col in range(puzzle.width):
+                            if puzzle[row][col] == key:
+                                self.grid.SetCellBackgroundColour(row,col,colour_dict[colour_def])
 
 if __name__ == '__main__':
-    puzzle = pycross.from_json(open("examples/5x5Coloured/camel.json").read())
+    puzzle = pycross.from_json(open("examples/WxHColoured/brocolli.json").read())
     cs = backtrack.constraint_search(puzzle)
     app = wx.App(0)
     frame = GridFrame(None)
-    frame.set_white(cs)
+    frame.setWhite(cs)
     frame.set_colour(cs)
     wx.Window.SetSize(frame,wx.DefaultCoord,wx.DefaultCoord,400,400)
     frame.Show()
