037f9f3482e4b5f62c2e0b2160ac42973d0738c4
adammcquade <48887237+adammcquade@users.noreply.github.com>
2021-04-30 23:58:18 +0100

    Update GeneticAlgorithm.py

--- pycross/GeneticAlgorithm.py
+++ pycross/GeneticAlgorithm.py
@@ -1,218 +1,188 @@
+
 import numpy as np
-import pycross
 from numpy import random
 import time
+import pycross
 
 class Solution:
-    def __init__(self, points, puzzleContraints):
-        self.points  = points
-        self.fitness = fitness(points, puzzleContraints)
+    def __init__(self, score, puzzleContraints):
+        self.score  = score
+        self.fitness = fitness(score, puzzleContraints)
 
-def getContraints(puzzle):
-    contraints = []
-    contraints = puzzle.rows
-    contraints = contraints.append(puzzle.columns)
-    return contraints
+class Game:
+    def __init__(self, rows, columns, score):
+        self.rows   = rows
+        self.columns = columns
+        self.board = []
+        self.difficulty = 1
+        self.colour = 1
 
-def main():
+        for row in range(rows):
+            line = []
+            for column in range(columns):
+                line = line + [False]
+            # effetively appends the new line to the puzzle
+            self.board = self.board + line
+
+class Contraints:
     
-    #x = pycross.from_json(open("examples/5x5Monochrome/1.json").read())
-    #puzzleName = "examples/5x5Monochrome/1.json"
-    #contraints = getContraints(x)
-    #print("contraints: ", contraints)
-    #populationSize = 200
-    
-
-    puzzleName = '1'
-    populationSize = 200
-
-    contraints = readContraintsFile('examples/' + puzzleName + '.txt')
-    puzzleContraints = createPuzzleContraints(contraints, populationSize)
-
-    #print("Puzzel constraints: ", puzzleContraints)
-
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
-    puzzleContraints = contraints, rows, columns, rows*columns, populationSize
-    
-    
-    pre = time.perf_counter()
-    solution = geneticAlgo(puzzleContraints)
-    print(checkSolution(Game(rows, columns, solution.points), contraints))
-
-    post = time.perf_counter()
-    print("Time: ", post - pre)
-   
+    def __init__(self, rows, columns):
+        self.rows   = rows
+        self.columns = columns
 
 def geneticAlgo(puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
-    iterations = 0
-    RandomAgents = randomInit(puzzleContraints)
-    
-    while not converge(RandomAgents, puzzleContraints):
-        ParentsChildren  = crossover(RandomAgents, puzzleContraints)
-        MutatedChildren = mutation(ParentsChildren, puzzleContraints)
-        RandomAgents   = select(RandomAgents, MutatedChildren, puzzleContraints)
-        iterations += 1
 
+# in this function we carry out the main genetic algorithm function. 
+# The operations mutate and crossOver are undertaken while the population has not
+# converged on a solution.
+#    
+    contraints, rows, columns, fitnessScore, populationSize = puzzleContraints
+    iterations = 0
+    maxIter = 1000
+    #initialise a population of agents which random configurations for the Picross puzzle.
+    
+    RandomAgents = []
+    for n in range(populationSize):
+        agent = []
+        for score in range(fitnessScore):
+            x = random.uniform(0, 1)
+            #after experimenting with this threshhold for setting filled values, it was determined that 40% is optimal.
+            threshhold = 0.4
+            if (x < threshhold):
+                agent = agent + [True]
+            else:
+                agent = agent + [False]
+        RandomAgents = RandomAgents + [Solution(agent, puzzleContraints)]
+    
+    for _ in range (maxIter):
+        # parents are selected from population and crossedover to produce offspirng.
+        ParentsChildren  = crossOver(RandomAgents, puzzleContraints)
+        # the  generation of children undergo mutations.
+        MutatedChildren = mutate(ParentsChildren, puzzleContraints)
+        # the next generation is selected based on a fitness function.
+        RandomAgents = select(RandomAgents, MutatedChildren, puzzleContraints)
+        
+        iterations += 1
         # print("Iteration count: ", iterations)
         # print("Contraints unmet: ", abs(RandomAgents[0].fitness))
-        if(iterations > 1000):
+
+        #the current generation is checked against the known solution.
+        #if the solution has been reached, the loop terminates early.
+        if (converge(RandomAgents, puzzleContraints)):
             break
     return finalIter(RandomAgents, puzzleContraints)
 
-def randomInit(puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
 
-    S = []
 
-    # print()
-    for _ in range(populationSize):
-        s = []
-        for _ in range(nPoints):
-            if random.random() <= 0.5:
-                s += [True]
-            else:
-                s += [False]
-        S += [Solution(s, puzzleContraints)]
-    return S
 
-def crossover(P, puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
+def crossOver(RandomAgents, puzzleContraints):
+    
+    contraints, rows, columns, fitnessScore, populationSize = puzzleContraints
+    ParentsChildren = []
 
-    PP    = []
+    RandomAgents = sorted(RandomAgents, key = lambda x : (x.fitness, random.random()))
+    
+    prob = [None] * 200
+    for i in range(200):
+        prob[i] = 1/(2*(i+1))
 
-    P = sorted(P, key = lambda s : (s.fitness, random.random()))
     n = (populationSize*(populationSize+1))/2
     prob=[i/n for i in range(1, populationSize+1)]
 
-    for _ in range(populationSize):
-        child1Points = []
-        child2Points = []
-        parent1, parent2 = random.choice(P, p=prob, replace=False, size=2)
+    for i in range(populationSize):
+        
+        newOffspring1 = []
+        newOffspring2 = []
+        newParent1 = random.choice(RandomAgents, p=prob)
+        newParent2 = random.choice(RandomAgents, p=prob)
+        #newOffspring1 = newOffspring1 + [newParent1.score[i]]
+        #newOffspring2 = newOffspring2 + [newParent2.score[i]]
+        
+        for i in range(fitnessScore):
+            x = random.uniform(0, 1)
 
-        for i in range(nPoints):
-            if random.random() <= 0.5:
-                child1Points += [parent1.points[i]]
-                child2Points += [parent2.points[i]]
+            if x < 0.5:
+                newOffspring1 = newOffspring1 + [newParent1.score[i]]
+                newOffspring2 = newOffspring2 + [newParent2.score[i]]
             else:
-                child1Points += [parent2.points[i]]
-                child2Points += [parent1.points[i]]
+                newOffspring1 = newOffspring1 + [newParent1.score[i]]
+                newOffspring2 = newOffspring2 + [newParent1.score[i]]
+        
+        ParentsChildren = ParentsChildren + [Solution(newOffspring1, puzzleContraints), Solution(newOffspring2, puzzleContraints)]
 
-        PP    += [Solution(child1Points, puzzleContraints), Solution(child2Points, puzzleContraints)]
+    return ParentsChildren
 
-    return PP
 
-def mutation(P, puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
+def mutate(RandomAgents, puzzleContraints):
+    contraints, rows, columns, fitnessScore, populationSize = puzzleContraints
+    #this is the operation of flipping filled or unfilled boxes in the nonogram.
 
-    PP = []
-
-    for s in P:
-
+    # this is the new generation of solutions
+    ParentsChildren = []
+    for agent in RandomAgents:
+        # this probability was derived from experimentation.
+        # Higher probabilities of mutation produce more volatile results, i.e. the solution may converge very quickly
+        # or may not converge at all depending on the initial random solutions.
+        
         prob = 0.01
-        newPoints = []
+        square = []
+        for score in agent.score:
+            x = random.uniform(0, 1)
 
-        for p in s.points:
-            if random.random() > prob:
-                newPoints += [p]
+            if x > prob:
+                square += [square]
             else:
-                newPoints += [not p]
+                square += [not square]
 
-        PP += [Solution(newPoints, puzzleContraints)]
+        ParentsChildren += [Solution(square, puzzleContraints)]
 
-    return PP
+    return ParentsChildren
 
-def select(randomAgents, crossoverParents, puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
+
+
+def select(randomAgents, MutatedChildren, puzzleContraints):
+    
+    contraints, rows, columns, fitnessScore, populationSize = puzzleContraints
 
     randomAgents = sorted(randomAgents, key = lambda s : (s.fitness, random.random()), reverse = True)
-    PP = sorted(crossoverParents, key = lambda s : (s.fitness, random.random()), reverse = True)
+    MutatedChildren = sorted(MutatedChildren, key = lambda s : (s.fitness, random.random()), reverse = True)
 
-    nParents  = int(0.2*populationSize)+1
+    nParents  = int(0.25*populationSize)+1
     nChildren = int(0.5*populationSize)+1
     nRandom = populationSize - nChildren - nParents
 
-    bestOnes = randomAgents[:nParents] + crossoverParents[:nChildren]
-    others   = randomAgents[nParents:] + crossoverParents[nChildren:]
+    bestOnes = randomAgents[:nParents] + MutatedChildren[:nChildren]
+    others   = randomAgents[nParents:] + MutatedChildren[nChildren:]
 
     nextGeneration = bestOnes + np.ndarray.tolist(random.choice(others, size=nRandom, replace=False))
 
     return nextGeneration
 
-def converge(P, puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
 
-    for s in P:
-        if s.fitness == 0:
-            return True
+def converge(RandomAgents, puzzleContraints):
+    contraints, rows, columns, fitnessScore, populationSize = puzzleContraints
+    # if we have converged on an optimal solution, we have converged, else we still continue the iterations.
 
-    for i in range(len(P)-1):
-        if P[i].points != P[i+1].points:
-            return False
+    for agent in RandomAgents:
+        if (agent.fitness == 0):
+            return True   
+    return False
 
-    return True
+def finalIter(RandomAgents, puzzleContraints):
+    contraints, rows, columns, fitnessScore, populationSize = puzzleContraints
 
-def finalIter(P, puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
+    bestAgent = RandomAgents[0]
+    for agent in RandomAgents:
+        if (agent.fitness == 0):
+            return agent
+        if (agent.fitness < bestAgent.fitness):
+            bestAgent = agent
+    
+    return bestAgent
 
-    for s in P:
-        if s.fitness == 0:
-            return s
-    return P[0]
-
-class Game:
-    def __init__(self, rows, columns, points):
-        self.rows   = rows
-        self.columns = columns
-        self.board = []
-
-        for _ in range(self.rows):
-            aux = []
-            for _ in range(self.columns):
-                aux += [False]
-            self.board += [aux]
-
-        self.fill(points, rows, columns)
-
-    def fill(self, points, rows, columns):
-        for i, v in enumerate(points):
-            self.board[int(i/columns)][i%columns] = v
-
-    def __str__(self):
-        result = '=' * ((self.columns)*2+2) + '\n'
-
-        for l in self.board:
-            result += '|'
-            for s in l:
-                result += (chr(9608) if not s else ' ')*2
-            result += '|\n'
-
-        result += '=' * ((self.columns)*2+2)
-        return result
-
-class Contraints:
-    # example: lines = [[1,2,3], [], [2], [9]]
-    def __init__(self, rows, columns):
-        self.rows   = rows
-        self.columns = columns
-
-    def __str__(self):
-        result = 'rows:\n'
-        for l in self.rows:
-            for n in l:
-                result += str(n)
-                result += ' '
-            result += '\n'
-
-        result += 'columns:\n'
-        for c in self.columns:
-            for n in c:
-                result += str(n)
-                result += ' '
-            result += '\n'
-        return result[:-1]
 
 def checkSolution(game, contraints):
+    
     board    = game.board
     rows   = game.rows
     columns = game.columns
@@ -252,7 +222,6 @@ def checkSolution(game, contraints):
         row = 0
         ruleIndex   = 0
 
-        # Check if all contraints are being fulfilled
         while row < rows and ruleIndex < contraintsQtt:
             countSegment = 0
 
@@ -269,11 +238,9 @@ def checkSolution(game, contraints):
 
             ruleIndex += 1
 
-        # Check if there isn't any remaining rule
         if ruleIndex < contraintsQtt:
             return False
 
-        # Check if there isn't any additional square after last rule
         while(row < rows):
             if(board[row][column]):
                 return False
@@ -281,44 +248,15 @@ def checkSolution(game, contraints):
 
     return True
 
-def readContraintsFile(puzzleFile):
-    
-    with open(puzzleFile) as puzzleFile:
-        readingLines = True
-        rows   = []
-        columns = []
 
-        for rule in puzzleFile:
-            if(rule == '-\n'):
-                readingLines = False
-                continue
-
-            constraints = [[int(x) for x in rule.split()]]
-            if(readingLines):
-                rows   += constraints
-            else:
-                columns += constraints
-
-    return Contraints(rows=rows, columns=columns)
-
-def createPuzzleContraints(contraints, populationSize):
-     
-    rows   = len(contraints.rows)
-    columns = len(contraints.columns)
-    nPoints  = 0
-
-    # Count total number of points
-    for row in contraints.rows:
-        for rule in row:
-            nPoints += rule
-
-    return (contraints, rows, columns, nPoints, populationSize)
 
 def fitness(sol, puzzleContraints):
-    contraints, rows, columns, nPoints, populationSize = puzzleContraints
+    
+    contraints, rows, columns, fitnessScore, populationSize = puzzleContraints
     count = 0
     game  = Game(rows, columns, sol)
     board = sol
+    #generic fitness function for nonograms sourced from github/rlegendi/griddler-solver
 
     for row in range(rows):
         contraintsQtt = len(contraints.rows[row])
@@ -366,9 +304,17 @@ import pycross
 
 def solve_from_picross(puzzle : pycross.Picross):
 
-    # contraints = readContraintsFile('examples/' + puzzleName + '.txt')
-    # puzzleContraints = createPuzzleContraints(contraints, populationSize)
-    # contraints, rows, columns, nPoints, populationSize = puzzleContraints
+
+    def split_to_lines(gen_list, width):
+        for i in range(0, len(gen_list), width):
+            yield gen_list[i:i + width]
+
+    def map_to_colour(marked):
+        if marked:
+            return list(puzzle.colours.keys())[0]
+        else:
+            return 0
+
 
     row_rules = [[rule[1] for rule in row] for row in puzzle.rows]
     column_rules = [[rule[1] for rule in column] for column in puzzle.columns]
@@ -381,30 +327,12 @@ def solve_from_picross(puzzle : pycross.Picross):
 
     pre = time.perf_counter()
     solution = geneticAlgo(puzzleContraints)
-    # print(checkSolution(Game(rows, columns, solution.points), contraints))
-    # print(Game(rows, columns, solution.points))
-    # print(solution.points)
     post = time.perf_counter()
-    # print("Time: ", post - pre)
-
-    def split_to_lines(gen_list, width):
-        for i in range(0, len(gen_list), width):
-            yield gen_list[i:i + width]
-
-    def map_to_colour(marked):
-        if marked:
-            return list(puzzle.colours.keys())[0]
-        else:
-            return 0
-
+    
     row = 0
-    for line in split_to_lines(solution.points, puzzle.width):
+    for line in split_to_lines(solution.score, puzzle.width):
         puzzle[row] = [map_to_colour(x) for x in line]
         row += 1
 
     return puzzle
-
-if __name__ == '__main__':
-    
-    main()
 
